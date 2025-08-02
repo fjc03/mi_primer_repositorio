9bbe93fa3a5eb3d6d41dbfac56572598def15701
fjc03 <fjcr.03@gmail.com>
2025-08-02 00:02:49 -0600

    Se agregacorreción para crear entorno virtual

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -2,9 +2,14 @@
 
 
 echo "Activando el entorno virtual"
+if [!"-d venv "]; then
+    python3 -m venv venv
+fi
+
 source venv/bin/activate
 
 echo "Instalando las dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Ejecutando las pruebas con pytest"
